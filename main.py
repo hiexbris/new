827622cb0b76bf7966ed3093178ec8994a7a9a71
hiexbris <hellojiaditya@gmail.com>
2025-01-05 14:33:53 +0530

    add pdfs

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-git 
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# vectorizer = TfidfVectorizer()
+# features = vectorizer.fit_transform(papers)
+
+# print(features.toarray())  
+
+import fitz  # PyMuPDF
+
+def extract_text_from_pdf(pdf_path):
+    # Open the PDF
+    doc = fitz.open(pdf_path)
+    text = ""
+    
+    for page_num in range(len(doc)):
+        page = doc.load_page(page_num) 
+        text += page.get_text()  
+    
+    return text
+
+pdf_path = "P001.pdf"
+paper_text = extract_text_from_pdf(pdf_path)
+print(paper_text)  
