a9b3b83c936d4f0857c2a951b94e59f759d4c01c
hiexbris <hellojiaditya@gmail.com>
2025-01-13 05:41:49 +0530

    before removing zero shot class

--- Training_Dataset.py
+++ Training_Dataset.py
@@ -4,7 +4,7 @@ classifier = Zero_shot()
 
 training_data = []
 
-for i in range(15):
+for i in range(30):
     if i < 5:
         paper = classifier.sections(f"D:\\KDAG Hackathon\\KDAG-Hackathon\\Train\\R00{i+1}.pdf")
         paper['label'] = 0
@@ -20,12 +20,16 @@ for i in range(15):
         print(f"{i+1}.Done")
     else:
         paper = classifier.sections(f"D:\\KDAG Hackathon\\KDAG-Hackathon\\Train\\R0{i+1}.pdf")
-        if i == 9 or i == 10:
+        if i == 9 or i == 10 or i == 21 or i == 22 or i == 23:
             paper['label'] = 3
-        elif i == 11 or i == 12:
+        elif i == 11 or i == 12 or i == 24 or i == 25 or i == 26:
             paper['label'] = 4
-        elif i == 13 or i == 14:
+        elif i == 13 or i == 14 or i == 27 or i == 28 or i == 29:
             paper['label'] = 5
+        elif i == 15 or i == 16 or i == 17:
+            paper['label'] = 1
+        elif i == 18 or i == 19 or i == 20:
+            paper['label'] = 2
         training_data.append(paper)
         print(f"{i+1}.Done")
 
